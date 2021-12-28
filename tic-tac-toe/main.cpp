93aa64592035b74c7c0e637615d8fb8d22fe725e
Mihir Gupta <guptamihir@protonmail.com>
2021-12-27 20:22:12 -0500

    Initial Commit

---
+++ tic-tac-toe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tic-tac-toe
+//
+//  Created by Mihir Gupta on 12/27/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
