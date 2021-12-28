9a0638d92485148f6ad1361646efdf4daec77f3a
Mihir Gupta <guptamihir@protonmail.com>
2021-12-27 20:42:37 -0500

    wrote the print_board function to print each of the b_arr values. initialized b_arr with blank space chars, checked if the print_board function is working as intended. it is.

--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -6,9 +6,33 @@
 //
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+void print_board();
+
+void place_peice();
+
+char b_arr[3][3] = {' ',' ',' ',' ',' '
+    ,' ',' ',' ',' '};
+
+int main() {
+    
+    print_board();
     return 0;
+}
+
+void print_board() {
+    cout << b_arr[0][0] << "|";
+    cout << b_arr[0][1] << "|";
+    cout << b_arr[0][2] << endl;
+    cout << "-----" << endl;
+    cout << b_arr[1][0] << "|";
+    cout << b_arr[1][1] << "|";
+    cout << b_arr[1][2] << endl;
+    cout << "-----" << endl;
+    cout << b_arr[2][0] << "|";
+    cout << b_arr[2][1] << "|";
+    cout << b_arr[2][2] << endl;
 }
